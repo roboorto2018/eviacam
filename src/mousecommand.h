aa04bd11f6e63dc3ae0d6098bb14051217621d2c
roboorto2018 <44677712+roboorto2018@users.noreply.github.com>
2026-03-15 22:50:57 +0100

    Sticky drag: aggiungi comandi CMD_LEFT/RIGHT_STICKY_DRAG

--- src/mousecommand.h
+++ src/mousecommand.h
@@ -1,37 +1,25 @@
 /////////////////////////////////////////////////////////////////////////////
 // Name:        mousecommand.h
-// Purpose:     mouse command definitions  
-// Author:      Cesar Mauri Loba (cesar at crea-si dot com)
-// Modified by:
-// Created:
-// Copyright:   (C) 2012 Cesar Mauri Loba - CREA Software Systems
-// 
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// Purpose:     mouse command definitions
+// Modified by: sticky drag patch
 /////////////////////////////////////////////////////////////////////////////
-
 #ifndef MOUSECOMMAND_H
 #define MOUSECOMMAND_H
 
 namespace mousecmd {
-	enum mousecmd { 
-		CMD_NO_CLICK = 0, 
-		CMD_LEFT_CLICK, 
-		CMD_MIDDLE_CLICK, 
-		CMD_RIGHT_CLICK, 
-		CMD_LEFT_DOWN, 
-		CMD_LEFT_UP, 
-		CMD_DOUBLE_CLICK 
+	enum mousecmd {
+		CMD_NO_CLICK = 0,
+		CMD_LEFT_CLICK,
+		CMD_MIDDLE_CLICK,
+		CMD_RIGHT_CLICK,
+		CMD_LEFT_DOWN,
+		CMD_LEFT_UP,
+		CMD_DOUBLE_CLICK,
+		// Sticky drag: button held while moving, released when stopped
+		CMD_LEFT_STICKY_DRAG,   // Activate/deactivate left sticky drag
+		CMD_RIGHT_STICKY_DRAG,  // Activate/deactivate right sticky drag
+		CMD_RIGHT_DOWN,
+		CMD_RIGHT_UP
 	};
 };
 
